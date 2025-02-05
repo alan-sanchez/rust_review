97b77ccdc1b14580177b066def1ca5624634023b
Alan G. Sanchez <sanchala@oregonstate.edu>
2025-02-04 16:09:58 -0800

    init commit

---
+++ projects/1. getting started/hello_world.rs
@@ -0,0 +1,12 @@
+// // The main funciton is special: It is always the first code that runs in every executable Rust program.
+// // If there were parameters, then they would go into the ().
+// // The function body is wrapped in {}.
+fn main() {
+    // Rust style is to indent with four spaces, not a tab! 
+
+    // // println! calls a Rust macro. 
+    println!("Hello, World!");
+}
+
+// // rustc [file name] compiles the code. After compiling successfully, rust outputs a binary executable.
+// // 
