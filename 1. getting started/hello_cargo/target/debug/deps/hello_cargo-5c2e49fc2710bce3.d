3b22f1e5566e36557ea782433c0d4ba64bff4753
Alan G. Sanchez <sanchala@oregonstate.edu>
2025-02-04 16:37:24 -0800

    Included cargo package example and restructured the files

---
+++ 1. getting started/hello_cargo/target/debug/deps/hello_cargo-5c2e49fc2710bce3.d
@@ -0,0 +1,5 @@
+/home/strider/catkin_ws/src/rust_review/1. getting started/hello_cargo/target/debug/deps/hello_cargo-5c2e49fc2710bce3: src/main.rs
+
+/home/strider/catkin_ws/src/rust_review/1. getting started/hello_cargo/target/debug/deps/hello_cargo-5c2e49fc2710bce3.d: src/main.rs
+
+src/main.rs:
---
+++ 1. getting started/hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/home/strider/catkin_ws/src/rust_review/1.\ getting\ started/hello_cargo/target/debug/hello_cargo: /home/strider/catkin_ws/src/rust_review/1.\ getting\ started/hello_cargo/src/main.rs
