c8b0cc0bdecdb2f0e03bd0e6967db066caf26c84
Alan G. Sanchez <sanchala@oregonstate.edu>
2025-02-07 15:37:18 -0800

    included loop and compare method to scrip

--- 2. guessing game/guessing_game/src/main.rs
+++ 2. guessing game/guessing_game/src/main.rs
@@ -7,6 +7,8 @@ use std::io;
 // // trait must be in scope for us to use those methods
 use rand::Rng;
 
+// // Calling the Odering type into scop from the standard library. The Odering type is another
+// // enum and has the variants Less, Greater, and Equal.
 use std::cmp::Ordering;
 
 
@@ -26,35 +28,45 @@ fn main() {
 
     println!("The secret number is: {secret_number}");
 
-    println!("Please input your guess.");
+    loop{
+        println!("Please input your guess.");
 
-    // // `let` statement is used to create a variable. Since variables are immutable by deafult, 
-    // // you need to make the variable mutabale by adding `mut` before the variable name
-    // // On the right of the equal sign, the variable `guess` is bound to a function that returns a new instance of a string. 
-    let mut guess = String::new();
+        // // `let` statement is used to create a variable. Since variables are immutable by deafult, 
+        // // you need to make the variable mutabale by adding `mut` before the variable name
+        // // On the right of the equal sign, the variable `guess` is bound to a function that returns a new instance of a string. 
+        let mut guess = String::new();
 
-    // '''
-    // The following block of text is part of a single logical line of code. 
-    // '''
-    // // There is the io module (I think similar to a class) that calls the `stdin` function,
-    // // which allows to handle user input
-    io::stdin()
-        // // `read_line` is a method on the standard input handle to get input from the user
-        // // We're also passing &mut guess as the argument to read_line to tell it what string to stroe the user input in
-        // // the `&` indicates that this argument is a reference to the variable guess
-        .read_line(&mut guess)
+        // '''
+        // The following block of text is part of a single logical line of code. 
+        // '''
+        // // There is the io module (I think similar to a class) that calls the `stdin` function,
+        // // which allows to handle user input
+        io::stdin()
+            // // `read_line` is a method on the standard input handle to get input from the user
+            // // We're also passing &mut guess as the argument to read_line to tell it what string to stroe the user input in
+            // // the `&` indicates that this argument is a reference to the variable guess
+            .read_line(&mut guess)
 
-        // // Will crash and give you a indicator why
-        .expect("Failed to read line");
+            // // Will crash and give you a indicator why
+            .expect("Failed to read line");
+            
         
-    
+        // // `trim()` removes any leading and trailing whitespace
+        // // Since we assigned the result to `let guess: u32`, Rust infers that `parse()` should convert the string to a u32
+        // // `.expect()` will terminate the program if the converstion failes and display the text message inside
         let guess: u32 = guess.trim().parse().expect("Please type a number!");
 
-    println!("You guessed: {}", guess);
+        println!("You guessed: {}", guess);
 
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("You win!"),
+        // // The `cmp` method compares two values and can be called on anything that can be compared. Here it 
+        // // compares `guess` to `secret_number`. Then ir treutns a variant of the Ordering enum we brought into scope
+        // //
+        // // We use a `match` expression to decide what to do next based on whcih variant of Ordering was returned. This
+        // // approach makes it more robust. 
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => println!("You win!"),
+        }
     }
 }
