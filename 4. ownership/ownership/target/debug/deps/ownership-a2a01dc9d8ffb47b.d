df7a609533b7a7640b200de449dc042fa8392a74
Alan G. Sanchez <sanchala@oregonstate.edu>
2025-02-17 16:10:15 -0800

    Init commit of ownership folder

---
+++ 4. ownership/ownership/target/debug/deps/ownership-a2a01dc9d8ffb47b.d
@@ -0,0 +1,5 @@
+/home/strider/catkin_ws/src/rust_review/4. ownership/ownership/target/debug/deps/ownership-a2a01dc9d8ffb47b: src/main.rs
+
+/home/strider/catkin_ws/src/rust_review/4. ownership/ownership/target/debug/deps/ownership-a2a01dc9d8ffb47b.d: src/main.rs
+
+src/main.rs:
---
+++ 4. ownership/ownership/target/debug/ownership.d
@@ -0,0 +1,1 @@
+/home/strider/catkin_ws/src/rust_review/4.\ ownership/ownership/target/debug/ownership: /home/strider/catkin_ws/src/rust_review/4.\ ownership/ownership/src/main.rs
