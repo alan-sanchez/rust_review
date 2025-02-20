9f6706d75f043642bff0e9db744c0fe93d920852
Alan G. Sanchez <sanchala@oregonstate.edu>
2025-02-19 22:49:38 -0800

    First example of reference

---
+++ 4. ownership/references/target/debug/deps/references-a46553eddf1a884c.d
@@ -0,0 +1,5 @@
+/home/strider/catkin_ws/src/rust_review/4. ownership/references/target/debug/deps/references-a46553eddf1a884c: src/main.rs
+
+/home/strider/catkin_ws/src/rust_review/4. ownership/references/target/debug/deps/references-a46553eddf1a884c.d: src/main.rs
+
+src/main.rs:
---
+++ 4. ownership/references/target/debug/references.d
@@ -0,0 +1,1 @@
+/home/strider/catkin_ws/src/rust_review/4.\ ownership/references/target/debug/references: /home/strider/catkin_ws/src/rust_review/4.\ ownership/references/src/main.rs
