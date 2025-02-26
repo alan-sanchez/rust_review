ff2f9b331a297bd02dd7be47fd7a8aa1d673ce83
Alan G. Sanchez <sanchala@oregonstate.edu>
2025-02-25 17:10:34 -0800

    Included dangling reference example

--- 4. ownership/references/src/main.rs
+++ 4. ownership/references/src/main.rs
@@ -16,6 +16,20 @@ fn main() {
     change(&mut s);
     println!("{s}");
 
+    // // This is not allowed because we cannont borrow s as mutalbe more than once at a time! 
+    // let r1 = &mut s;
+    // let r2 = &mut s;
+
+    // // We also cannot have amutable reerecne while we have a immutable one to the ssame value
+    // let r1 = &s; // no problem
+    // let r2 = &s; // no problem
+    // let r3 = &mut s; // BIG PROBLEM
+
+
+    // // Dangling References // // 
+    let reference_to_nothing = dangle();
+    println!("{reference_to_nothing}")
+
 }
 
 // // Function that takes a reference to a String (`s: &String`) and returns its length.
@@ -25,7 +39,13 @@ fn calculate_length(s: &String) -> usize {
   // // it refers to, the value is not dropped.
 
 
-  // // 
+  // // we create a mutable reference with &mut s where we call the change function, and update the function signature to 
+  // // accept a mutable reference with some_string: &mut String. This makes it very clear that the change function will mutate the value it borrows.
   fn change(some_string: &mut String) {
     some_string.push_str(", mundo");
+  }
+
+  fn dangle() -> String {
+    let new_s = String::from("hello");
+    new_s
   }
