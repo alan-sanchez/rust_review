b6b28ff7fe48fcaf0bf4348bafa7eaf8bf4eb5b9
Alan G. Sanchez <sanchala@oregonstate.edu>
2025-02-25 16:45:31 -0800

    Included mutable reference example

--- 4. ownership/references/src/main.rs
+++ 4. ownership/references/src/main.rs
@@ -1,16 +1,31 @@
 fn main() {
-    // Create a new String variable `s1` and initialize it with "hello".
+    // // Create a new String variable `s1` and initialize it with "hello".
     let s1 = String::from("hello");
 
-    // Pass a reference to `s1` to the `calculate_length` function.
-    // This allows `calculate_length` to borrow `s1` without taking ownership.
+    // // Pass a reference to `s1` to the `calculate_length` function. the `&s1` syntax lets us create a reference
+    // // that refers to the value of s1 but does not own it. 
     let len = calculate_length(&s1);
 
-    // Print the original string and its calculated length.
+    // // Print the original string and its calculated length.
     println!("The length of '{s1}' is {len}.");
+
+
+    // // Mutable Reference // // 
+    let mut s = String::from("Hola");
+
+    change(&mut s);
+    println!("{s}");
+
 }
 
-// Function that takes a reference to a String (`s: &String`) and returns its length.
+// // Function that takes a reference to a String (`s: &String`) and returns its length.
 fn calculate_length(s: &String) -> usize {
     s.len()  // `.len()` method returns the number of bytes in the String.
-}
+} // // Here, s goes out of scope. But because s does not have ownership of what
+  // // it refers to, the value is not dropped.
+
+
+  // // 
+  fn change(some_string: &mut String) {
+    some_string.push_str(", mundo");
+  }
