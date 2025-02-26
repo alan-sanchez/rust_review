48ff6fdb6ab93ffe66649dff86d706ef78893e7a
Alan G. Sanchez <sanchala@oregonstate.edu>
2025-02-26 15:10:20 -0800

    Init commit of structs lesson

---
+++ 5. structs/structs/target/debug/deps/structs-4d39280d65fb1a40.d
@@ -0,0 +1,5 @@
+/home/strider/catkin_ws/src/rust_review/5. structs/structs/target/debug/deps/structs-4d39280d65fb1a40: src/main.rs
+
+/home/strider/catkin_ws/src/rust_review/5. structs/structs/target/debug/deps/structs-4d39280d65fb1a40.d: src/main.rs
+
+src/main.rs:
---
+++ 5. structs/structs/target/debug/structs.d
@@ -0,0 +1,1 @@
+/home/strider/catkin_ws/src/rust_review/5.\ structs/structs/target/debug/structs: /home/strider/catkin_ws/src/rust_review/5.\ structs/structs/src/main.rs
