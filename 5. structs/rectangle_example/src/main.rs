2e29a88a62aa6e3a7389957971b8ee5b1b1f9c65
Alan G. Sanchez <sanchala@oregonstate.edu>
2025-03-05 12:24:27 -0800

    Included Refactoring function with struct argument

--- 5. structs/rectangle_example/src/main.rs
+++ 5. structs/rectangle_example/src/main.rs
@@ -11,30 +11,52 @@ fn main() {
         area(width1, height1)
     );
 
-
     // // Create tuple message type
-    let rect1 = (30,50);
+    let rect2 = (30,50);
 
     // // Print values
     println!(
-        "The area of the second rectantgle is {} square pixels.", 
-        area_2(rect1)
+        "The area of the second rectangle is {} square pixels.", 
+        area_2(rect2)
     );
+
+
+    // // Define struct
+    let rect3 = Rectangle{
+        width: 30,
+        height: 50,
+    };
+
+    println!(
+        "The area of the third rectangle is {}",
+        area_3(&rect3)
+    );
+
 }
 
-// Define a function named `area` that calculates the area of a rectangle.
-// It takes two parameters:
-// - `width`: the width of the rectangle (of type u32)
-// - `height`: the height of the rectangle (of type u32)
-// The function returns a u32, which is the product of `width` and `height`.
+// // Create struct to help label data
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+// // Define a function named `area` that calculates the area of a rectangle.
+// // It takes two parameters:
+// // - `width`: the width of the rectangle (of type u32)
+// // - `height`: the height of the rectangle (of type u32)
+// // The function returns a u32, which is the product of `width` and `height`.
 fn area(width: u32, height: u32) -> u32 {
     // // Multiply width and height to compute the area.
     width * height
 }
 
-
 // // Refactoring code with Tuples
 fn area_2(dimensions:(u32,u32)) -> u32 {
     // // To call the information from the tuple, you must use index values
     dimensions.0 * dimensions.1
+}
+
+// // Refactoring function with Struct argument
+fn area_3(rectangle: &Rectangle) -> u32 {
+    rectangle.width * rectangle.height
 }
