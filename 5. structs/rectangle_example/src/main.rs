e6b7c86160fbb5c083ced5e7f9ae598f7084c8c1
Alan G. Sanchez <sanchala@oregonstate.edu>
2025-03-05 11:56:20 -0800

    Included Tuple approach as arguments for a function

--- 5. structs/rectangle_example/src/main.rs
+++ 5. structs/rectangle_example/src/main.rs
@@ -1,14 +1,24 @@
 fn main() {
-    // Define the width of the rectangle
+    // //  Define the width of the rectangle
     let width1 = 30;
-    // Define the height of the rectangle
+    // // Define the height of the rectangle
     let height1 = 50;
 
-    // Call the `area` function with `width1` and `height1` as arguments,
-    // then print the returned area value within a formatted string.
+    // // Call the `area` function with `width1` and `height1` as arguments,
+    // // then print the returned area value within a formatted string.
     println!(
         "The area of the rectangle is {} square pixels.",
         area(width1, height1)
+    );
+
+
+    // // Create tuple message type
+    let rect1 = (30,50);
+
+    // // Print values
+    println!(
+        "The area of the second rectantgle is {} square pixels.", 
+        area_2(rect1)
     );
 }
 
@@ -18,6 +28,13 @@ fn main() {
 // - `height`: the height of the rectangle (of type u32)
 // The function returns a u32, which is the product of `width` and `height`.
 fn area(width: u32, height: u32) -> u32 {
-    // Multiply width and height to compute the area.
+    // // Multiply width and height to compute the area.
     width * height
+}
+
+
+// // Refactoring code with Tuples
+fn area_2(dimensions:(u32,u32)) -> u32 {
+    // // To call the information from the tuple, you must use index values
+    dimensions.0 * dimensions.1
 }
